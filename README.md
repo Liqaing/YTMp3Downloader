428aa51ae4d59cec8c8c1ed1b6b84655523504b2
Rithy <huotrithy01@gmail.com>
2023-07-18 18:50:27 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Youtube Mp3 Downloader
---
+++ YTMp3Downloader/mp3downloader/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Mp3DownloaderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mp3downloader'
