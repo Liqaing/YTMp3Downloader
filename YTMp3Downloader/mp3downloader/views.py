59eab4231f4c4b96196d84444916f8c7921200ef
Liqaing <huotrithy01@gmail.com>
2023-08-27 18:13:21 +0700

    1. use url to download, 2. download on server then to client and remove on server

--- YTMp3Downloader/mp3downloader/views.py
+++ YTMp3Downloader/mp3downloader/views.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 import yt_dlp
 
 from . import util
@@ -17,19 +18,23 @@ def index(request):
         # Retrive video id
         yt_video_id = util.get_yt_video_id(yt_url)
         if not yt_video_id:
-            # Cannot retrive the video id
             return render(request, "mp3downloader/index.html")
 
+        # Download video as mp3
         ydl_opts = {
             'format': 'bestaudio/best',
             'postprocessors': [{
                 'key': 'FFmpegExtractAudio',
                 'preferredcodec': 'mp3',
                 'preferredquality': '192',
-            }]
+            }],
+            # 'outtmpl': r'C:\Users\Rick\Downloads',
         }
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([yt_video_id])
+        
+        ydl = yt_dlp.YoutubeDL(ydl_opts)
+        info_dict = ydl.extract_info(yt_video_id, download=True)
+
+        print(info_dict)
 
         return render(request, "mp3downloader/index.html")
 
